6493dcc89cd90289e82d9db09ea8c1dc8587f514
Alexandr <opera9876@rambler.ru>
2023-12-31 13:26:22 +0300

    more info on sum

--- sum.go
+++ sum.go
@@ -1,5 +1,8 @@
 package sum
 
+// Add sums up 2 integer numbers and returns its result,
+// feel free to sum any 32bit numbers.
+// To learn more check this: https://www.mathsisfun.com/numbers/addition.html
 func Add(num1, num2 int) int {
     return num2 + num1
 }
